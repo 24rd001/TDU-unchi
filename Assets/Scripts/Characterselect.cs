9f1507a9cfe85348542aaafb05aff5dd23f73f5b
24rd001 <a.felmest14.gg@gmail.com>
2026-06-15 17:54:09 +0900

    選択画面からキャラクター選択可能

--- Assets/Scripts/Characterselect.cs
+++ Assets/Scripts/Characterselect.cs
@@ -4,31 +4,31 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelect : MonoBehaviour
 {
-    [SerializeField] RectTransform highlight;          // 金枠
-    [SerializeField] RectTransform[] cards;            // Card0〜2
-    [SerializeField] TMP_Text tokuchoText;             // とくちょう欄
-    [TextArea][SerializeField] string[] descriptions;  // 説明文(3個)
+    [SerializeField] RectTransform highlight;
+    [SerializeField] RectTransform[] cards;
+    [SerializeField] TMP_Text tokuchoText;
+    [TextArea][SerializeField] string[] descriptions;
 
     int index = 0;
 
-    void Start() => Show(0);
+    void Start() => Select(0);
 
-    public void Hover(int i)  => Show(i);        // ★ホバーで金枠も文章も動く
-    public void Unhover()     => Show(index);    // 離れたら選択中に戻る
-    public void Select(int i) { index = i; Show(i); } // クリックで選択を確定
+    public void Hover(int i)  => Select(i);   // ★ホバーで選択も確定
+    public void Unhover()     { }             // ★離れても、最後に合わせたキャラを保持
+    public void Select(int i) { index = i; Show(i); }  // クリックでもOK
 
     void Show(int i)
     {
-        if (highlight) highlight.position = cards[i].position; // ★金枠を移動
-        tokuchoText.text = descriptions[i];                    // 文章を更新
+        if (highlight) highlight.position = cards[i].position;
+        tokuchoText.text = descriptions[i];
     }
 
     public void Confirm()
     {
         GameData.SelectedCharacter = index;
         Debug.Log("決定：" + index);
-        SceneManager.LoadScene("MouseScene"); // GameSceneを作ったら有効化
+        SceneManager.LoadScene("MouseScene");
     }
 
-    public void Back() => SceneManager.LoadScene("TitleScene"); // ★もどる→タイトル
+    public void Back() => SceneManager.LoadScene("TitleScene");
 }
