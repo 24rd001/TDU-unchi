bf1932b2a7a087ba0443296eec7d86844eaca12e
jojo3394 <shirajo9812@gmail.com>
2026-07-06 15:31:03 +0900

    level

--- Assets/Scripts/Gage/NutritionItem.cs
+++ Assets/Scripts/Gage/NutritionItem.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class NutritionItem : MonoBehaviour
 {
     public float value = 15;
+    public float expValue = 10;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             StatusManager.Instance.AddNutrition(value);
+
+            LevelManager.Instance.AddExp(expValue);
+
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    public int level = 1;
+
+    public float currentExp = 0;
+    public float requiredExp = 100;
+
+    [Header("UI")]
+    public TMP_Text levelText;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+
+        if (levelText != null)
+        {
+            levelText.text = "Lv." + level;
+        }
+    }
+
+    public void AddExp(float amount)
+    {
+        currentExp += amount;
+
+        while (currentExp >= requiredExp)
+        {
+            currentExp -= requiredExp;
+
+            level++;
+
+            requiredExp *= 1.2f;
+
+            Debug.Log("レベルアップ！");
+        }
+    }
+}
